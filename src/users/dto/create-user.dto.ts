0d28cc12c4ebeb2b14a5988d0bfba003d69b1334
Iram Saba Khan <161953870+iramsk02@users.noreply.github.com>
2025-10-05 17:40:40 +0530

    made changes to implement admin endpoints

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,27 +1,3 @@
-// import { IsEmail, IsOptional, IsString, MinLength, IsBoolean } from 'class-validator';
-
-// export class CreateUserDto {
-//   @IsEmail()
-//   email: string;
-
-//   @IsOptional()
-//   @IsString()
-//   @MinLength(6)
-//   password?: string;
-
-//   @IsOptional()
-//   @IsString()
-//   firstName?: string;
-
-//   @IsOptional()
-//   @IsString()
-//   lastName?: string;
-
-//   @IsOptional()
-//   @IsBoolean()
-//   isEmailVerified?: boolean;
-// }
-
 import { IsEmail, IsOptional, IsString, MinLength, IsBoolean, IsEnum } from 'class-validator';
 import { UserRole } from '../user.entity';
 
